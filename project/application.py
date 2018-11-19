c6e37180514e1708bccc26de6488415bb40d0ec4
Budmonde Duinkharjav <budmonde@gmail.com>
2018-11-18 20:31:36 -0500

    Add base arpeggiator implementation g fe

--- project/application.py
+++ project/application.py
@@ -1,4 +1,5 @@
 import sys
+import random
 sys.path.append('..')
 from common.core import *
 from common.audio import *
@@ -8,6 +9,7 @@ from common.clock import *
 from common.metro import *
 from arpeggiator import *
 
+# part 1: create Arpeggiator (in pset4_arpeg.py) and test it here
 class MainWidget1(BaseWidget) :
     def __init__(self):
         super(MainWidget1, self).__init__()
@@ -66,12 +68,99 @@ class MainWidget1(BaseWidget) :
         self.label.text += 'u i o p: Change Rhythm\n'
         self.label.text += '1 2 3: Change Direction\n'
 
+C7 = [60, 64, 67, 70]
+C7MAJ = [60, 64, 67, 71]
+C7MIN = [60, 63, 67, 70]
+CHORD_BASES = [C7.copy(), C7MAJ.copy(), C7MIN.copy()]
+C7_BEBOP = [60, 62, 64, 65, 67, 69, 70, 71]
+
+def AUGMENT(base, aug):
+    return [p+aug for p in base]
+
+def ARPEGIATE(base, aug, octva, length):
+    return [p-12*(octva-o) for o in range(length) for p in AUGMENT(base, aug)]
+
+def ALLOCATE(base, aug, octva, length, skip):
+    return [ARPEGIATE(base, aug, octva, length)[i*skip:i*skip+len(base)] for i in range(12)]
+
+def GET_CHORDS_LIST(aug, ctype):
+    return ARPEGIATE(CHORD_BASES[ctype], aug, 3, 2)
+
+def GET_NOTES_LIST(aug):
+    return ALLOCATE(C7_BEBOP, aug, 2, 6, 2)
+
+# Chord Progression for Dolphin Shoals
+CHORD_PROGRESSION = [
+    (0, 1, 4), # augmentation, chord type, num beats
+    (2, 2, 4),
+    (4, 2, 4),
+    (7, 0, 4),
+    # repeat
+    (0, 1, 4),
+    (2, 2, 4),
+    (4, 2, 4),
+    (7, 0, 4),
+    # A'
+    (2, 2, 4),
+    (7, 0, 4),
+    (3, 2, 4),
+    (8, 0, 4),
+    # end
+    (2, 2, 2),
+    (4, 2, 2),
+    (7, 0, 4),
+    (2, 2, 2),
+    (4, 2, 2),
+    (7, 0, 4),
+    # B
+    (0, 1, 8),
+    (5, 1, 8),
+    (0, 1, 8),
+    (5, 1, 8),
+]
+
+BASS_RHYTHM = (480, 1)
+RHYTHMS_LIST = [(120*2**(5-i), 1) for i in range(6)]
+
+DIRECTION_LIST = ["up", "down"]
+SWITCH_CHANCE = 0.01
+
+class Bubble(InstructionGroup):
+    def __init__(self, pos, r, color):
+        super(Bubble, self).__init__()
+
+        self.radius_anim = KFAnim((0, 0), (.1, 5*r), (.9, r))
+
+        self.color = Color(hsv=color)
+        self.add(self.color)
+
+        self.circle = CEllipse(cpos = pos, size = (2*r, 2*r), segments = 40)
+        self.add(self.circle)
+
+        self.time = 0
+        self.on_update(0)
+
+    def on_update(self, dt):
+        # advance time
+        self.time += dt
+
+        rad = self.radius_anim.eval(self.time)
+        self.circle.csize = (2*rad, 2*rad)
+
+        v = Window.size[1] / 3
+        pos = (self.circle.cpos[0], self.circle.cpos[1] + v * dt)
+        self.circle.cpos = pos
+        return pos[1] > 0
 
 # Part 2, 3, and 4
 class MainWidget2(BaseWidget) :
     def __init__(self):
         super(MainWidget2, self).__init__()
+        # Graphics
+        self.objects = AnimGroup()
+        self.canvas.add(self.objects)
 
+        # Audio
         self.audio = Audio(2)
         self.synth = Synth('../data/FluidR3_GM.sf2')
 
@@ -83,31 +172,84 @@ class MainWidget2(BaseWidget) :
         self.audio.set_generator(self.sched)
         self.sched.set_generator(self.synth)
 
+        self.beat = 0
+        self.chord_idx = 0
+        self.chord_list = GET_CHORDS_LIST(*CHORD_PROGRESSION[self.chord_idx][:-1])
+        self.notes_list = GET_NOTES_LIST(CHORD_PROGRESSION[self.chord_idx][0])
+
+        # create a drumkit
+        self.drums = Arpeggiator(self.sched, self.synth, channel = 0, patch = (128, 0))
+        self.drums.set_notes([35, 42, 38, 42])
+        self.drums.set_rhythm(*BASS_RHYTHM)
+
+        # create the bass:
+        self.bass = Arpeggiator(self.sched, self.synth, channel = 1, patch = (0,32), callback = self._add_beat)
+        self.bass.set_notes(self.chord_list)
+        self.bass.note_idx = 15
+        self.bass.set_rhythm(*BASS_RHYTHM) 
+
+        # create the arpeggiator:
+        self.arpeg = JazzArpeggiator(self.sched, self.synth, channel = 2, patch = (0,65), callback = self._gen)
+
         # and text to display our status
         self.label = topleft_label()
         self.add_widget(self.label)
 
+    def _add_beat(self):
+        self.beat += 1
+        if CHORD_PROGRESSION[self.chord_idx][2] == self.beat - 1:
+            self.chord_idx = (self.chord_idx+1) % len(CHORD_PROGRESSION)
+            self.chord_list = GET_CHORDS_LIST(*CHORD_PROGRESSION[self.chord_idx][:-1])
+            self.bass.set_notes(self.chord_list)
+            self.notes_list = GET_NOTES_LIST(CHORD_PROGRESSION[self.chord_idx][0])
+            self.beat = 0
+
+    def _gen(self, pitch, length):
+        # Graphics
+        p = ((pitch - 35) / 50) * Window.width, max(Window.height - length / 3, 0)
+        r = 10
+        c = ((pitch - 35) / 50,1,1)
+        self.objects.add(Bubble(p, r, c))
+
     def on_key_down(self, keycode, modifiers):
-        pass
+        if keycode[1] == 's':
+            self.drums.toggle()
+            self.bass.toggle()
 
     def on_key_up(self, keycode):
         pass
 
     def on_touch_down(self, touch):
-        pass
+        notes_idx = int(touch.pos[0] / (Window.width / len(self.notes_list)))
+        rhythms_idx = int(touch.pos[1] / (Window.height / len(RHYTHMS_LIST)))
+
+        self.arpeg.set_notes(self.notes_list[notes_idx])
+        self.arpeg.set_rhythm(*RHYTHMS_LIST[rhythms_idx])
+
+        self.arpeg.start()
 
     def on_touch_up(self, touch):
+        self.arpeg.stop()
         pass
 
     def on_touch_move(self, touch):
-        pass
+        notes_idx = int(touch.pos[0] / (Window.width / len(self.notes_list)))
+        rhythms_idx = int(touch.pos[1] / (Window.height / len(RHYTHMS_LIST)))
+
+        self.arpeg.set_notes(self.notes_list[notes_idx])
+        self.arpeg.set_rhythm(*RHYTHMS_LIST[rhythms_idx])
 
     def on_update(self) :
+        self.objects.on_update()
         self.audio.on_update()
         self.label.text = self.sched.now_str() + '\n'
 
+        if random.uniform(0,1) < SWITCH_CHANCE:
+            direction = random.choice(DIRECTION_LIST)
+            self.arpeg.set_direction(direction)
+        if random.uniform(0,1) < SWITCH_CHANCE:
+            direction = random.choice(DIRECTION_LIST)
+            self.bass.set_direction(direction)
 
 # pass in which MainWidget to run as a command-line arg
-#run(eval('MainWidget' + sys.argv[1]))
-run(eval('MainWidget' + '1'))
-
+run(eval('MainWidget' + sys.argv[1]))
--- project/arpeggiator.py
+++ project/arpeggiator.py
@@ -1,30 +1,69 @@
+import random
 from common.clock import kTicksPerQuarter, quantize_tick_up
 
 
 class Arpeggiator(object):
     def __init__(self, sched, synth, channel=0, patch=(0, 40), callback = None):
         super(Arpeggiator, self).__init__()
-        
         self.sched = sched
         self.synth = synth
         self.channel = channel
         self.patch = patch
         self.callback = callback
-        
-        self.notes = []
-        self.is_playing = False
-        self.note_grid = 480
-        self.note_len_ratio = 1
-        self.direction = ''
+
+        self.notes = None
+        self.note_len = 0
+        self.incr = 1
+        self.reflect = False
+
+        # run-time variables
+        self.note_idx = 0
+        self.on_cmd = None
+        self.off_cmd = None
+        self.playing = False
 
     # start the arpeggiator
     def start(self):
-        self.is_playing = True
-        self.play()
+        if self.playing:
+            return
+
+        self.note_idx = 0
+        self.playing = True
+
+        # set up the correct sound (program change)
+        self.synth.program(self.channel, self.patch[0], self.patch[1])
+
+        # find the tick of the next beat, and make it "beat aligned"
+        now = self.sched.get_tick()
+        next_beat = quantize_tick_up(now, kTicksPerQuarter)
+
+        # now, post the _noteon function (and remember this command)
+        self.on_cmd = self.sched.post_at_tick(self._noteon, next_beat)
     
     # stop the arpeggiator
     def stop(self):
-        self.is_playing = False
+        if not self.playing:
+            return 
+            
+        self.playing = False
+
+        # in case there is a note on hanging, turn it off immediately
+        if self.off_cmd:
+            self.off_cmd.execute()
+
+        # cancel anything pending in the future.
+        self.sched.remove(self.on_cmd)
+        self.sched.remove(self.off_cmd)
+
+        # reset these so we don't have a reference to old commands.
+        self.on_cmd = None
+        self.off_cmd = None
+
+    def toggle(self):
+        if self.playing:
+            self.stop()
+        else:
+            self.start()
     
     # notes is a list of MIDI pitch values. For example [60 64 67 72]
     def set_notes(self, notes):
@@ -34,13 +73,162 @@ class Arpeggiator(object):
     # note_len_ratio defines the duration of the note. For example 0.5 will 
     # make notes last a duration of note_grid/2
     def set_rhythm(self, note_grid, note_len_ratio):
-        self.note_grid = note_grid
-        self.note_len_ratio = note_len_ratio
+        self.note_len = note_grid * note_len_ratio
 
     # dir is either 'up', 'down', or 'updown'
     def set_direction(self, direction):
-        self.direction = direction 
+        self.incr = 1 if direction != 'down' else -1
+        self.reflect = True if direction == 'updown' else False
 
-    def play(self):
-        if len(self.notes) == 0 or self.direction == '':
-            raise Exception('Must set the notes and direction before playing')
+    def _noteon(self, tick, ignore):
+        # avoid race condition bug
+        self.note_idx = min(self.note_idx, len(self.notes)-1)
+
+        # pick pitch
+        note_pitch = self.notes[self.note_idx]
+        if self.reflect:
+            if self.note_idx == 0 or self.note_idx == (len(self.notes) - 1):
+                self.incr *= -1
+        self.note_idx = (self.note_idx + self.incr) % len(self.notes)
+
+        # play the note right now:
+        self.synth.noteon(self.channel, note_pitch, 100)
+
+        # post the note off for half a beat later:
+        self.off_cmd = self.sched.post_at_tick(self._noteoff, tick + self.note_len, note_pitch)
+
+        # schedule the next noteon for one beat later
+        next_beat = tick + self.note_len
+        self.on_cmd = self.sched.post_at_tick(self._noteon, next_beat)
+
+        # callback for note_event
+        if self.callback is not None:
+            self.callback()
+
+    def _noteoff(self, tick, pitch):
+        # just turn off the currently sounding note.
+        self.synth.noteoff(self.channel, pitch)
+
+class JazzArpeggiator(object):
+    def __init__(self, sched, synth, channel=0, patch=(0, 40), callback = None):
+        super(JazzArpeggiator, self).__init__()
+        self.sched = sched
+        self.synth = synth
+        self.channel = channel
+        self.patch = patch
+        self.callback = callback
+
+        self.notes = None
+        self.note_len = 0
+        self.incr = 1
+
+        # run-time variables
+        self.note_idx = 0
+        self.on_cmd = None
+        self.off_cmd = None
+        self.playing = False
+
+        self.beat = 0
+
+    # start the arpeggiator
+    def start(self):
+        if self.playing:
+            return
+
+        self.note_idx = 0
+        self.playing = True
+
+        # set up the correct sound (program change)
+        self.synth.program(self.channel, self.patch[0], self.patch[1])
+
+        # find the tick of the next beat, and make it "beat aligned"
+        now = self.sched.get_tick()
+        next_beat = quantize_tick_up(now, kTicksPerQuarter)
+
+        # now, post the _noteon function (and remember this command)
+        self.on_cmd = self.sched.post_at_tick(self._noteon, next_beat)
+    
+    # stop the arpeggiator
+    def stop(self):
+        if not self.playing:
+            return 
+            
+        self.playing = False
+
+        # in case there is a note on hanging, turn it off immediately
+        if self.off_cmd:
+            self.off_cmd.execute()
+
+        # cancel anything pending in the future.
+        self.sched.remove(self.on_cmd)
+        self.sched.remove(self.off_cmd)
+
+        # reset these so we don't have a reference to old commands.
+        self.on_cmd = None
+        self.off_cmd = None
+
+    def toggle(self):
+        if self.playing:
+            self.stop()
+        else:
+            self.start()
+    
+    # notes is a list of MIDI pitch values. For example [60 64 67 72]
+    def set_notes(self, notes):
+        self.notes = notes
+    
+    # note_grid is the speed of the notes. For example 240 is 1/8th notes.
+    # note_len_ratio defines the duration of the note. For example 0.5 will 
+    # make notes last a duration of note_grid/2
+    def set_rhythm(self, note_grid, note_len_ratio):
+        self.note_len = note_grid * note_len_ratio
+
+    # dir is either 'up', 'down', or 'updown'
+    def set_direction(self, direction):
+        self.incr = 1 if direction != 'down' else -1
+
+    def _noteon(self, tick, ignore):
+        # avoid race condition bug
+        self.note_idx = min(self.note_idx, len(self.notes)-1)
+
+        # pick pitch, length and velocity
+        note_pitch = self.notes[self.note_idx]
+        note_len = self.note_len
+        velocity = 100
+        incr = self.incr
+
+        # probablistic changes
+        if self.beat == 0:
+            if random.uniform(0,1) < 0.3: # accented downbeat
+                velocity *= 1.5
+        if self.beat % 2 == 0:
+            if random.uniform(0,1) < 0.3: # chromatic approach
+                note_len /= 2
+                note_pitch -= incr
+                self.note_idx -= incr
+            if random.uniform(0,1) < 0.4: # hold strong beat
+                note_len *= 2
+        if random.uniform(0,1) < 0.2:
+            self.note_idx = (self.note_idx - incr * random.randint(1,5)) % len(self.notes)
+
+        self.note_idx = (self.note_idx + incr) % len(self.notes)
+
+        # play the note right now:
+        self.synth.noteon(self.channel, note_pitch, velocity)
+
+        # post the note off for half a beat later:
+        self.off_cmd = self.sched.post_at_tick(self._noteoff, tick + note_len, note_pitch)
+
+        # schedule the next noteon for one beat later
+        next_beat = tick + note_len
+        self.on_cmd = self.sched.post_at_tick(self._noteon, next_beat)
+
+        # callback for note_event
+        if self.callback is not None:
+            self.callback(note_pitch, note_len)
+
+        self.beat = (self.beat + 1) % 4
+
+    def _noteoff(self, tick, pitch):
+        # just turn off the currently sounding note.
+        self.synth.noteoff(self.channel, pitch)
